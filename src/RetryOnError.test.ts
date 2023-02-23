47c85f2b8441c0ba13b8d787330b971c81a84627
Shahar Har-Shuv <sharshuv@quotient.com>
2023-02-23 16:31:23 -0500

    Remove lodash dependency

---
+++ src/RetryOnError.test.ts
@@ -0,0 +1,137 @@
+import { RetryOnError } from './RetryOnError';
+import {
+  Observable,
+  throwError,
+  of
+} from 'rxjs';
+
+describe(RetryOnError.name, () => {
+  it('should retry the decorated method when an error is thrown', async () => {
+    // Define a class with a decorated method that returns an error the first time it is called
+    let count = 0;
+
+    class TestClass {
+      @RetryOnError()
+      testMethod(): Observable<number> {
+        count++;
+        if (count === 1) {
+          return throwError(() => new Error('Test error'));
+        } else {
+          return of(42);
+        }
+      }
+    }
+
+    // Create an instance of the class and call the decorated method
+    const instance = new TestClass();
+    const result = await instance.testMethod().toPromise();
+
+    // Verify that the decorated method was retried and returned the expected result
+    expect(count).toBe(2);
+    expect(result).toBe(42);
+  });
+
+  it('should stop retrying after the maximum number of retries is reached', async () => {
+    // Define a class with a decorated method that always throws an error
+    let count = 0;
+
+    class TestClass {
+      @RetryOnError()
+      testMethod(): Observable<number> {
+        count++;
+        return throwError(new Error('Test error'));
+      }
+    }
+
+    // Create an instance of the class and call the decorated method
+    const instance = new TestClass();
+
+    // Verify that the decorated method throws an error after the maximum number of retries is reached
+    await expect(instance.testMethod().toPromise()).rejects.toThrow(
+      'Test error',
+    );
+    expect(count).toBe(4);
+  });
+
+  it('should not retry the decorated method if the error is not filtered', async () => {
+    // Define a class with a decorated method that always throws an error
+    let count = 0;
+
+    class TestClass {
+      @RetryOnError({
+        errorFilter: (error) => error instanceof TypeError,
+      })
+      testMethod(): Observable<number> {
+        count++;
+        return throwError(new Error('Test error'));
+      }
+    }
+
+    // Create an instance of the class and call the decorated method
+    const instance = new TestClass();
+
+    // Verify that the decorated method throws an error immediately without retrying
+    expect(instance.testMethod().toPromise()).rejects.toThrow('Test error');
+    expect(count).toBe(1);
+  });
+
+  it('should delay between retries', async () => {
+    // Define a class with a decorated method that returns an error the first time it is called
+    let count = 0;
+    const startTime = new Date().getTime();
+
+    const delay = 100;
+
+    class TestClass {
+      @RetryOnError({
+        retryDelay: delay,
+      })
+      testMethod(): Observable<number> {
+        count++;
+        if (count === 1) {
+          return throwError(new Error('Test error'));
+        } else {
+          return of(42);
+        }
+      }
+    }
+
+    // Create an instance of the class and call the decorated method
+    const instance = new TestClass();
+    await instance.testMethod().toPromise();
+
+    // Verify that the decorated method was retried with a delay between retries
+    const endTime = new Date().getTime();
+    const duration = endTime - startTime;
+    expect(duration).toBeGreaterThanOrEqual(delay);
+  });
+
+  it('should call the onRetry function when a retry occurs', async () => {
+    // Define a class with a decorated method that returns an error the first time it is called
+    let count = 0;
+    let message = '';
+
+    class TestClass {
+      @RetryOnError({
+        onRetry: (error, retryCount) => {
+          message = `Retrying (${retryCount}) due to error: ${error.message}`;
+        },
+      })
+      testMethod(): Observable<number> {
+        count++;
+        if (count === 1) {
+          return throwError(new Error('Test error'));
+        } else {
+          return of(42);
+        }
+      }
+    }
+
+    // Create an instance of the class and call the decorated method
+    const instance = new TestClass();
+    await instance.testMethod().toPromise();
+
+    // Verify that the onRetry function was called with the expected arguments
+    expect(message).toBe('Retrying (1) due to error: Test error');
+  });
+});
---
+++ src/RetryOnError.ts
@@ -0,0 +1,47 @@
+import {
+  Observable,
+  throwError,
+} from 'rxjs';
+import {
+  catchError,
+  delay,
+} from 'rxjs/operators';
+import { createMethodDecoratorFromHighOrderFn } from 'method-decorators-utils';
+
+export function RetryOnError<
+  Fn extends (...args: any[]) => Observable<any>
+>(options?: {
+  maxRetries?: number;
+  retryDelay?: number;
+  errorFilter?: (error: any) => boolean;
+  onRetry?: (error: any, retryCount: number) => void;
+}) {
+  const _options: Required<NonNullable<typeof options>> = {
+    maxRetries: 3,
+    retryDelay: 1000,
+    errorFilter: () => true,
+    onRetry: () => {
+    },
+    ...(options || {}),
+  };
+
+  return createMethodDecoratorFromHighOrderFn((childFunction) => {
+    return function(...args: Parameters<Fn>): ReturnType<Fn> {
+      function getObservable(retryCount: number = 0): ReturnType<Fn> {
+        return childFunction(...args).pipe(
+          catchError((error: any) => {
+            if (!_options.errorFilter(error) || retryCount >= _options.maxRetries) {
+              return throwError(error);
+            }
+
+            _options.onRetry(error, retryCount + 1);
+
+            return getObservable(retryCount + 1).pipe(delay(_options.retryDelay));
+          }),
+        );
+      }
+
+      return getObservable();
+    };
+  });
+}
